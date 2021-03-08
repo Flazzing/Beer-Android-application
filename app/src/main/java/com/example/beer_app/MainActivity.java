507157b0d022fb6e962fe52b08e967581fbd4f50
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2021-03-07 22:56:24 -0800

    main screen, random beer button screen

--- app/src/main/java/com/example/beer_app/MainActivity.java
+++ app/src/main/java/com/example/beer_app/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.beer_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button rando_beer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rando_beer = (Button) findViewById(R.id.random_beer_button);
+        rando_beer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("MainActivity", "clicked random beer");
+        Intent intent = new Intent(this, RandoBeer.class);
+        startActivity(intent);
     }
 }
